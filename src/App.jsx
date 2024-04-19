2fc601cd35e7e660a5c62d984add522444a59ade
Dominik Lewicki <dominik_03-06@wp.pl>
2024-04-19 20:29:15 +0100

    ffff

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import O1 from "./components/O1";
 import O2 from "./components/O2";
+import O3 from "./components/O3";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 <Navbar/>
 <O1/>
 <O2/>
+<O3/>
     </div>
   );
 }
---
+++ src/components/O3.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './O3.css';
+
+const O3 = () => {
+  return (
+   <div className="o3">
+    <div className="k3">
+
+    </div>
+   </div>
+  )
+}
+
+export default O3
