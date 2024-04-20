218f9c0470b674937fa554f2245d217c3595e3d0
Dominik Lewicki <dominik_03-06@wp.pl>
2024-04-20 19:01:24 +0100

    ffff

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import O1 from "./components/O1";
 import O2 from "./components/O2";
 import O3 from "./components/O3";
+import O4 from "./components/O4";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 <O1/>
 <O2/>
 <O3/>
+<O4/>
     </div>
   );
 }
---
+++ src/components/O4.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./O4.css";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const O4 = () => {
+  return (
+<div className="o4">
+  <div className='na-txt4'>Recent news</div>
+    <div className="k4">
+        <div className="pion">
+            <div className="kwa44">01</div>
+            <div className="kwa44">02</div>
+            <div className="kwa44">03</div>
+        </div>
+        <div className="na-pro">
+<div className="pro4">Cortex raises $35M Series B for its internal developer portal</div>
+<div className="pro4">Captions raises $25 Million to bring AI to creators</div>
+<div className="pro4">
+Robust Intelligence to be named most innovative company in data science of 2023
+</div>
+</div>
+    </div>
+    <div className="na-btn4">
+        <button className='btn4'>Read more news <MdOutlineKeyboardArrowRight  size={30}/>
+
+</button>
+    </div>
+</div>
+  )
+}
+
+export default O4
