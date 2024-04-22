bf63a070be619239f19bb47d60d3881282004f9d
Dominik Lewicki <dominik_03-06@wp.pl>
2024-04-22 16:29:15 +0100

    ffff

--- src/components/O4.jsx
+++ src/components/O4.jsx
@@ -4,34 +4,25 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-
 const O4 = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-  
     gsap.fromTo(
       '.txt4',
-      { y: 100, opacity: 0 },
-      { y: 0, opacity: 1, duration: 0.3, ease: 'power2.out', scrollTrigger: { trigger: '.pro4', start: 'top 90%', end: 'bottom center' } }
+      { y: 200, opacity: 0 },
+      { y: 0, opacity: 1, duration: 0.6,  ease: 'power2.out', scrollTrigger: { trigger: '.pro4', start: 'top 90%', end: 'bottom center' } }
     );
-  
     gsap.fromTo(
       '.txt44',
-      { y: 100, opacity: 0 },
-      { y: 0, opacity: 1, duration: 0.3, ease: 'power2.out', scrollTrigger: { trigger: '.pro44', start: 'top 90%', end: 'bottom center' } }
-    );
-  
+      { y: 200, opacity: 0 },
+      { y: 0, opacity: 1, duration: 0.7,  ease: 'power2.out', scrollTrigger: { trigger: '.pro44', start: 'top 90%', end: 'bottom center' } }
+    )
     gsap.fromTo(
       '.txt444',
-      { y: 100, opacity: 0 },
-      { y: 0, opacity: 1, duration: 0.3, ease: 'power2.out', scrollTrigger: { trigger: '.pro444', start: 'top 90%', end: 'bottom center' } }
+      { y: 200, opacity: 0 },
+      { y: 0, opacity: 1, duration: 0.8, ease: 'power2.out', scrollTrigger: { trigger: '.pro444', start: 'top 90%', end: 'bottom center' } }
     );
   }, []);
-  
-
-
-
-
   return (
 <div className="o4">
   <div className='na-txt4'>Recent news</div>
@@ -41,38 +32,20 @@ const O4 = () => {
             <div className="kwa44">02</div>
             <div className="kwa44">03</div>
         </div>
-
-
-
-
-
-
-        <div className="na-pro">
-
-          
+        <div className="na-pro">  
 <div className="pro4">
   <p className='txt4'>Cortex raises $35M Series B for its internal developer portal</p>
   </div>
-  
+
 <div className="pro4-display">
   <p className='txt4-display'>Cortex raises $35M Series B for its internal developer portal</p>
 </div>
-
-
-
-
-
-
-
-  
 <div className="pro44">
   <p className='txt44'>Captions raises $25 Million to bring AI to creators </p>
   </div>
 <div className="pro4-display">
 <p className='txt4-display'>Captions raises $25 Million to bring AI to creators </p>
 </div>
-
-
 <div className="pro444">
   <p className='txt444'>
 Robust Intelligence to be named most innovative company in data science of 2023
@@ -84,23 +57,12 @@ Robust Intelligence to be named most innovative company in data science of 2023
 </p>
 </div>
 </div>
-
-
-
-
-
-
-
-
-
     </div>
     <div className="na-btn4">
         <button className='btn4'>Read more news <MdOutlineKeyboardArrowRight  size={30}/>
-
 </button>
     </div>
 </div>
   )
 }
-
 export default O4
--- src/components/O5.jsx
+++ src/components/O5.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import './O5.css';
 import { CgArrangeBack } from "react-icons/cg";
 import { CgCarousel } from "react-icons/cg";
 import { CgBriefcase } from "react-icons/cg";
 import { CgDigitalocean } from "react-icons/cg";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 
 const O5 = () => {
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.fromTo('.pro5',
+          { opacity: 0 },
+          {  opacity: 1, duration: 1,  ease: 'power2.out', scrollTrigger: { trigger: '.pro5', start: 'top 80%', end: 'bottom center' } }
+        );
+        gsap.fromTo('.pro55',
+          {  opacity: 0 },
+          {  opacity: 1, duration: 1,  ease: 'power2.out', scrollTrigger: { trigger: '.pro55', start: 'top 90%', end: 'bottom center' } }
+        );
+        gsap.fromTo('.pro555',
+          {  opacity: 0 },
+          { opacity: 1, duration: 1, ease: 'power2.out', scrollTrigger: { trigger: '.pro555', start: 'top 90%', end: 'bottom center' } }
+        );
+        gsap.fromTo('.pro5555',
+        {  opacity: 0 },
+        { opacity: 1, duration: 1, ease: 'power2.out', scrollTrigger: { trigger: '.pro5555', start: 'top 90%', end: 'bottom center' } }
+      );
+      }, []);
+
+
+
+      useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.fromTo('.pion55',
+          { opacity: 0 },
+          {  opacity: 1, duration: 1,  ease: 'power2.out', scrollTrigger: { trigger: '.pion55', start: 'top 80%', end: 'bottom center',  } }
+        );
+        gsap.fromTo('.pion555',
+          {  opacity: 0 },
+          {  opacity: 1, duration: 1,  ease: 'power2.out', scrollTrigger: { trigger: '.pion555', start: 'top 90%', end: 'bottom center' } }
+        );
+        gsap.fromTo('.pion5555',
+          {  opacity: 0 },
+          { opacity: 1, duration: 1, ease: 'power2.out', scrollTrigger: { trigger: '.pion5555', start: 'top 90%', end: 'bottom center' } }
+        );
+        gsap.fromTo('.pion55555',
+        {  opacity: 0 },
+        { opacity: 1, duration: 1, ease: 'power2.out', scrollTrigger: { trigger: '.pion55555', start: 'top 90%', end: 'bottom center',  } }
+      );
+      }, []);
+
+
+
   return (
 <div className="o5">
     <div className="k5">
@@ -21,11 +68,11 @@ const O5 = () => {
     <div className="pion5">
         <div className="pion55"><CgArrangeBack />
 </div>
-        <div className="pion55"><CgCarousel />
+        <div className="pion555"><CgCarousel />
 </div>
-        <div className="pion55"><CgBriefcase />
+        <div className="pion5555"><CgBriefcase />
 </div>
-        <div className="pion55"><CgDigitalocean />
+        <div className="pion55555"><CgDigitalocean />
 </div>
     </div>
     <div className="na-pro5">
@@ -38,7 +85,7 @@ const O5 = () => {
 <br></br>
 <p className='txt5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis unde dicta sit dignissimos iusto illum cum totam eligendi necessitatibus est praesentium corporis laudantium sunt minus, quibusdam assumenda veritatis, aperiam explicabo eum ipsam repudiandae vitae! Hic explicabo autem necessitatibus aut dolorem.</p>
     </div>
-    <div className="pro5">
+    <div className="pro55">
     <p className='small-color-txt'>INTEGRATED APPROACH
 
 </p>
@@ -49,7 +96,7 @@ const O5 = () => {
 <br></br>
 <p className='txt5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis unde dicta sit dignissimos iusto illum cum totam eligendi necessitatibus est praesentium corporis laudantium sunt minus, </p>
     </div>
-    <div className="pro5">
+    <div className="pro555">
     <p className='small-color-txt'>FOUNDER FOCUSED
 </p>
 <br></br>
@@ -58,7 +105,7 @@ const O5 = () => {
 <br></br>
 <p className='txt5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis unde dicta sit dignissimos iusto illum cum totam eligendi necessitatibus est praesentium corporis laudantium sunt minus Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perspiciatis a qui cum necessitatibus eaque possimus tempora culpa dolor temporibus?, </p>
     </div>
-    <div className="pro5">
+    <div className="pro5555">
     <p className='small-color-txt'>FOUNDER FOCUSED
 </p>
 <br></br>
